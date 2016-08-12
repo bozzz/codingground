731a575f4c40fb9245c28db91be5af1b41a63136
bozzz <b_oz@live.com>
2016-08-12 11:23:36 +0200

    Created by Coding Ground Client

---
+++ Bab - GPS/main.py
@@ -0,0 +1,19 @@
+# -*- coding: latin-1 -*-
+
+def conversion(old):
+    direction = {'N':1, 'S':-1, 'E': 1, 'W':-1}
+    new = old.replace(u'°',' ').replace('\'',' ').replace('"',' ')
+    new = new.split()
+    try:
+        x = int(new[0])
+        new_dir = new.pop()
+    except:
+        new_dir = new.pop(0)
+   
+    new.extend([0,0,0])
+    return (float(new[0])+float(new[1])/60.0+float(new[2])/3600.0) * direction[new_dir]
+
+#lat, lon = u'''0°25'30"S, 91°7'W'''.split(', ')
+lat, lon = u'''N 60° 26.457', E 15° 29.518'''.split(', ')
+
+print conversion(lat), conversion(lon)
